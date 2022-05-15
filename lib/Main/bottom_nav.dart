558753f24a4545c76711b2b60ad1afa82c9dbc22
aorohith <aorohith@gmail.com>
2022-05-15 11:15:15 +0530

    Added flutter toast for double press of back button for exit

--- lib/Main/bottom_nav.dart
+++ lib/Main/bottom_nav.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:visual_magic/FavouriteScreen/favourites_screen.dart';
 import 'package:visual_magic/FolderScreen/folders_screen.dart';
 import 'package:visual_magic/Main/showcase_inheritted.dart';
@@ -40,38 +41,56 @@ class _BottomNavbarState extends State<BottomNavbar> {
     super.initState();
   }
 
+  DateTime timeBackPressed = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return KeysToBeInherited(
-      key1: _key1,
-      key2: _key2,
-      key3: _key3,
-      key4: _key4,
-      child: Scaffold(
-          bottomNavigationBar: CurvedNavigationBar(
-            key: _bottomNavigationKey,
-            index: 0,
-            height: 60.0,
-            items: [
-              Icon(Icons.folder, size: 30),
-              Icon(Icons.history, size: 30),
-              Icon(Icons.play_circle, size: 30),
-              Icon(Icons.favorite, size: 30),
-            ],
-            color:Color.fromARGB(255, 81, 73, 150),
-            buttonBackgroundColor: Colors.white,
-            backgroundColor: Color(0xff060625),
-            animationCurve: Curves.easeInOut,
-            animationDuration: Duration(milliseconds: 600),
-            onTap: (index) {
-              setState(() {
-                _page = index;
-              });
-            },
-            letIndexChange: (index) => true,
-          ),
-          body: _pages[_page]),
+    return WillPopScope(
+      onWillPop: ()async {
+        final difference = DateTime.now().difference(timeBackPressed);
+        final isExitWarning = difference >= Duration(seconds: 2);
+
+        timeBackPressed = DateTime.now();
+
+        if(isExitWarning){
+          final message = 'Press back again to Exit';
+          Fluttertoast.showToast(msg: message, fontSize: 18);
+          return false;
+        }else{
+          Fluttertoast.cancel();
+          return true;
+        }
+      },
+      child: KeysToBeInherited(
+        key1: _key1,
+        key2: _key2,
+        key3: _key3,
+        key4: _key4,
+        child: Scaffold(
+            bottomNavigationBar: CurvedNavigationBar(
+              key: _bottomNavigationKey,
+              index: 0,
+              height: 60.0,
+              items: [
+                Icon(Icons.folder, size: 30),
+                Icon(Icons.history, size: 30),
+                Icon(Icons.play_circle, size: 30),
+                Icon(Icons.favorite, size: 30),
+              ],
+              color:Color.fromARGB(255, 81, 73, 150),
+              buttonBackgroundColor: Colors.white,
+              backgroundColor: Color(0xff060625),
+              animationCurve: Curves.easeInOut,
+              animationDuration: Duration(milliseconds: 600),
+              onTap: (index) {
+                setState(() {
+                  _page = index;
+                });
+              },
+              letIndexChange: (index) => true,
+            ),
+            body: _pages[_page]),
+      ),
     );
   }
 }
