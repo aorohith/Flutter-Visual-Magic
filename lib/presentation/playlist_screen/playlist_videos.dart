1d28cef073a9f770776e14f72a9340ec0b3942c4
aorohith <aorohith@gmail.com>
2022-06-12 20:04:37 +0530

    Design totally changed

--- lib/presentation/playlist_screen/playlist_videos.dart
+++ lib/presentation/playlist_screen/playlist_videos.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:visual_magic/core/colors/colors.dart';
 import 'package:visual_magic/db/Models/models.dart';
 import 'package:visual_magic/main.dart';
 import 'package:visual_magic/presentation/widgets/empty_display_text.dart';
 import 'package:visual_magic/presentation/widgets/popup_button.dart';
-
 import '../menu_drawer/menu_drawer.dart';
 import '../video_player/video_player.dart';
 import 'widgets/playlist_videos_popup.dart';
 
-List<String>? fetchedVideos;
 
 class PlaylistVideos extends StatefulWidget {
   final namePlay;
@@ -26,9 +25,9 @@ class _PlaylistVideosState extends State<PlaylistVideos> {
     double _w = MediaQuery.of(context).size.width;
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
       drawer: const MenuDrawer(),
       floatingActionButton: playButton(context),
-      backgroundColor: const Color(0xff060625),
       appBar: AppBar(
         title: Text(widget.namePlay),
         actions: [
@@ -65,75 +64,74 @@ class _PlaylistVideosState extends State<PlaylistVideos> {
                 ),
         ],
       ),
-      body: AnimationLimiter(
-        child: ValueListenableBuilder(
-          valueListenable: playListVideosDB.listenable(),
-          builder: (BuildContext ctx, Box<PlayListVideos> playListVideos,
-              Widget? child) {
-            return playListVideos.isEmpty
-                ? emptyDisplay("${widget.namePlay} Videos")
-                : ListView.builder(
-                    padding: EdgeInsets.all(_w / 30),
-                    physics: const BouncingScrollPhysics(
-                        parent: AlwaysScrollableScrollPhysics()),
-                    itemCount: playListVideos.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      PlayListVideos? playlistVideo =
-                          playListVideos.getAt(index);
+      body: Container(
+        decoration: bgColor,
+        child: Padding(
+          padding: const EdgeInsets.only(top:90.0),
+          child: AnimationLimiter(
+            child: ValueListenableBuilder(
+              valueListenable: playListVideosDB.listenable(),
+              builder: (BuildContext ctx, Box<PlayListVideos> playListVideos,
+                  Widget? child) {
+                return playListVideos.isEmpty
+                    ? emptyDisplay("${widget.namePlay} Videos")
+                    : ListView.builder(
+                        padding: EdgeInsets.all(_w / 30),
+                        physics: const BouncingScrollPhysics(
+                            parent: AlwaysScrollableScrollPhysics()),
+                        itemCount: playListVideos.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          PlayListVideos? playlistVideo =
+                              playListVideos.getAt(index);
 
-                      return playlistVideo!.playListName == widget.namePlay
-                          ? Container(
-                            margin: EdgeInsets.only(bottom: _w / 20),
-                            height: _w / 5,
-                            decoration: BoxDecoration(
-                              color: const Color(0xff1f1f55),
-                              borderRadius: const BorderRadius.all(
-                                Radius.circular(10),
-                              ),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.1),
-                                  blurRadius: 40,
-                                  spreadRadius: 10,
+                          return playlistVideo!.playListName == widget.namePlay
+                              ? Container(
+                                margin: EdgeInsets.only(bottom: _w / 20),
+                                height: _w / 5,
+                                decoration: const BoxDecoration(
+                                  color: listColor,
+                                  borderRadius:  BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
                                 ),
-                              ],
-                            ),
-                            child: Center(
-                              child: ListTile(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => VideoPlay(
-                                        videoLink:
-                                            playlistVideo.playListVideo,
-                                      ),
+                                child: Center(
+                                  child: ListTile(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => VideoPlay(
+                                            videoLink:
+                                                playlistVideo.playListVideo,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    leading: Image.asset(
+                                        "assets/images/download.jpeg"),
+                                    title: Text(
+                                      playlistVideo.playListVideo
+                                          .split("/")
+                                          .last,
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
                                     ),
-                                  );
-                                },
-                                leading: Image.asset(
-                                    "assets/images/download.jpeg"),
-                                title: Text(
-                                  playlistVideo.playListVideo
-                                      .split("/")
-                                      .last,
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
+                                    trailing: PlayVideosPopup(
+                                      videoPath:
+                                          playlistVideo.playListVideo,
+                                      playlistName:
+                                          playlistVideo.playListName,
+                                    ),
+                                  ),
                                 ),
-                                trailing: PlayVideosPopup(
-                                  videoPath:
-                                      playlistVideo.playListVideo,
-                                  playlistName:
-                                      playlistVideo.playListName,
-                                ),
-                              ),
-                            ),
-                          )
-                          : const SizedBox();
-                    },
-                  );
-          },
+                              )
+                              : const SizedBox();
+                        },
+                      );
+              },
+            ),
+          ),
         ),
       ),
     );
